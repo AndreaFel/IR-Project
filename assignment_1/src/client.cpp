fcaaf3c2096e1ec750d7c9f755b118272c24e067
AndreaFel <and.felline@gmail.com>
2023-12-13 15:45:01 +0100

    Added rotation after movement

--- assignment_1/src/client.cpp
+++ assignment_1/src/client.cpp
@@ -1,12 +1,8 @@
 #include "ros/ros.h"
 #include "move_base_msgs/MoveBaseActionGoal.h"
+#include "move_base_msgs/MoveBaseActionResult.h"
 #include <tf2/LinearMath/Quaternion.h>
 #include <tf2_geometry_msgs/tf2_geometry_msgs.h>
-
-// Function prototype for rotating 360 degrees
-void rotate360Degrees(ros::Publisher& publisher) {
-    // TODO: move here the code for the Rotation of 360 degrees
-}
 
 int main(int argc, char **argv) {
     
@@ -60,21 +56,50 @@ int main(int argc, char **argv) {
         // Publish goal
         goal_publisher.publish(goal_msg);
         ROS_INFO("Goal sent!");
-
-        // Perform a 360-degree rotation (Currently commented out)
-        // rotate360Degrees(goal_publisher);
-
-        // Set quaternion for 360-degree rotation
-        tf2::Quaternion quat;
-        // 6.28319 radians = 360 degrees
-        quat.setRPY(0, 0, 6.28319);
-        tf2::convert(quat, goal_msg.goal.target_pose.pose.orientation);
-
-        // Publish rotation goal
-        goal_publisher.publish(goal_msg);
-        ROS_INFO("Rotation sent!");
-    }
+		
+		// Wait for result
+		move_base_msgs::MoveBaseActionResult::ConstPtr received_msg;
+		received_msg = ros::topic::waitForMessage<move_base_msgs::MoveBaseActionResult>("/move_base/result", nh);
+		
+		// Check result
+		if(received_msg->status.status==3)
+			ROS_INFO("Movement completed!");
+		else
+			ROS_INFO("Movement failed!");
+		
+		// Perform a 360-degree rotation (Currently commented out)
+		ROS_INFO("Starting rotation...");
+		
+        int status = 0; // To check that every action goes well
+        int counter = 1; // Rotate of 120° for 3 times
+        
+		do{
+			// Define rotation of counter*120° degrees as quaternion
+			tf2::Quaternion quat_rotation;
+			quat_rotation.setRPY(0, 0, counter*2*M_PI/3);
+			goal_msg.goal.target_pose.pose.orientation.x = quat_rotation.x();
+			goal_msg.goal.target_pose.pose.orientation.y = quat_rotation.y();
+			goal_msg.goal.target_pose.pose.orientation.z = quat_rotation.z();
+			goal_msg.goal.target_pose.pose.orientation.w = quat_rotation.w();
+			
+			// Publish goal
+			goal_publisher.publish(goal_msg);
+			
+			// Wait for result before sending the next one
+			received_msg = ros::topic::waitForMessage<move_base_msgs::MoveBaseActionResult>("/move_base/result", nh);
+			
+			// Update status and counter
+			status = received_msg->status.status;
+			counter++;
+			
+		}while(status==3 && counter<=3);
+		
+		// Check rotation status
+		if(status==3)
+			ROS_INFO("Rotation completed!");
+		else
+			ROS_INFO("Rotation failed!");
+	}
 
     return 0;
 }
-
